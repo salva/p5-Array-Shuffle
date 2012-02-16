3ecfdb8ed09f14b17efa5ac814f0179b2f2631c7
Salvador <sfandino@yahoo.com>
2012-02-16 18:51:07 +0100

    working on it

--- Shuffle.xs
+++ Shuffle.xs
@@ -7,3 +7,40 @@
 
 MODULE = Array::Shuffle		PACKAGE = Array::Shuffle		
 
+PROTOTYPES: DISABLE
+
+BOOT:
+#if (PERL_VERSION >= 14)
+    fprintf(stderr, "hello 5.14\n"); fflush(stderr);
+    sv_setpv((SV*)GvCV(gv_fetchpvs("Array::Shuffle::shuffle_array", 0, SVt_PVCV)), "+");
+#else
+    sv_setpv((SV*)GvCV(gv_fetchpvs("Array::Shuffle::shuffle_array", 0, SVt_PVCV)), "+");
+#endif
+
+void
+shuffle_array(av)
+    AV *av
+CODE:
+    if (SvRMAGICAL(av)) {
+        IV i;
+        for (i = av_len(av); i > 0; i--) {
+            int j = (i + 1) * Drand01();
+            SV **svpi = av_fetch(av, i, 0);
+            SV *svi = (svpi ? newSVsv(*svpi) : &PL_sv_undef);
+            SV **svpj = av_fetch(av, j, 0);
+            SV *svj = (svpj ? newSVsv(*svpj) : &PL_sv_undef);
+            if (!av_store(av, i, svj)) SvREFCNT_dec(svj);
+            if (!av_store(av, j, svi)) SvREFCNT_dec(svi);
+        }
+    }
+    else {
+        SV **svp = AvARRAY(av);
+        IV i;
+        for (i = av_len(av); i > 0; i--) {
+            int j = (i + 1) * Drand01();
+            SV *tmp = svp[i];
+            svp[i] = svp[j];
+            svp[j] = tmp;
+        }
+    }
+                      
--- lib/Array/Shuffle.pm
+++ lib/Array/Shuffle.pm
@@ -1,86 +1,44 @@
 package Array::Shuffle;
 
-use 5.014002;
+our $VERSION = '0.01';
+
 use strict;
 use warnings;
-
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Array::Shuffle ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
-our $VERSION = '0.01';
 
 require XSLoader;
 XSLoader::load('Array::Shuffle', $VERSION);
 
-# Preloaded methods go here.
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(shuffle_array);
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Array::Shuffle - Perl extension for blah blah blah
+Array::Shuffle - fast shuffling of arrays in-place
 
 =head1 SYNOPSIS
 
-  use Array::Shuffle;
-  blah blah blah
+  use Array::Shuffle qw(shuffle_array);
+  shuffle_array(@a);
 
 =head1 DESCRIPTION
 
-Stub documentation for Array::Shuffle, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
-
+This module implements the C<shuffle_array> function for shuffling and
+array in-place.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
-
-=head1 AUTHOR
-
-Salvador Fandino, E<lt>salva@E<gt>
+L<List::Util>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2012 by Salvador Fandino
+Copyright (C) 2012 by Salvador FandiE<ntilde>o (sfandino@yahoo.com)
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.14.2 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
